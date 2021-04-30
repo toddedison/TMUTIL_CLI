933b1f687f933a1c728c3948fb76d15c6b72cfbb
Michael Horty <michael.horty@outlook.com>
2021-04-29 22:19:02 -0400

    ssss

---
+++ TMutil/modCommon.vb
@@ -0,0 +1,630 @@
+﻿Imports System
+Imports Newtonsoft.Json
+Imports System.Security.Cryptography
+Imports System.IO
+
+
+Module modCommon
+
+    Public Sub addLoginCreds(ByRef AA As TM_Client, uN$, pW$)
+        Dim S3 As New Simple3Des("7w6e87twryut24876wuyeg")
+
+        ' if exists, don't add
+        Dim LL As Collection
+        LL = getLogins()
+
+        If grpNDX(LL, AA.tmFQDN + "|" + S3.Encode(uN) + "|" + S3.encode(pW)) Then Exit Sub
+
+        Dim FF As Integer
+        FF = FreeFile()
+
+        Dim sKeyU$ = uN
+        Dim sKeyP$ = pW
+
+        sKeyU = S3.Encode(sKeyU)
+        sKeyP = S3.Encode(sKeyP)
+
+        FileOpen(FF, "logins", OpenMode.Append)
+        Print(FF, AA.tmFQDN + "|" + sKeyU + "|" + sKeyP + Chr(13))
+        FileClose(FF)
+
+        S3 = Nothing
+    End Sub
+
+    Public Function getLogins() As Collection
+        getLogins = New Collection
+
+        If Dir("logins") = "" Then Exit Function
+
+        Dim FF As Integer
+        FF = FreeFile()
+        Dim a$
+
+        FileOpen(FF, "logins", OpenMode.Input)
+
+        Do Until EOF(FF) = True
+            a$ = LineInput(FF)
+            If InStr(a, "|") Then getLogins.Add(a)
+        Loop
+
+        FileClose(FF)
+    End Function
+
+
+    Public Function csvTOquotedList(ByVal a$, Optional ditchQuotes As Boolean = False) As String
+        Dim b$ = ""
+
+        Dim C As Object
+        C = Split(a, ",")
+
+        Dim d$
+        d = Chr(34)
+        If ditchQuotes = True Then d = ""
+
+        Dim K As Integer
+        For K = 0 To UBound(C)
+            b += d + C(K) + d + ","
+        Next
+
+        b = Mid(b, 1, Len(b) - 1)
+        Return b$
+    End Function
+
+
+    Public Function argValue(lookForArg$, ByRef theArgs$()) As String
+        ' assumes format "--arg value"
+        Dim argNum As Integer = 0
+
+        For Each A In theArgs
+            argNum += 1
+            If LCase(A) = LCase(lookForArg) Then
+                If argNum + 1 > theArgs.Count Then Return ""
+                Return theArgs(argNum)
+            End If
+        Next
+        Return ""
+    End Function
+
+    Public Function argExist(lookForArg$, ByRef theArgs$()) As String
+        ' assumes format "--arg value"
+        argExist = False
+
+        For Each A In theArgs
+            If LCase(A) = LCase(lookForArg) Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+
+    Public Function grpNDX(ByRef C As Collection, ByRef a$, Optional ByVal caseSensitive As Boolean = True) As Integer
+        Dim K As Long
+        grpNDX = 0
+
+        If C.Count = 0 Then Exit Function
+        If caseSensitive = False Then GoTo dontEvalCase
+
+        For K = 1 To C.Count
+            If a = C(K) Then
+                grpNDX = K
+                Exit Function
+            End If
+        Next
+        Exit Function
+
+dontEvalCase:
+        For Each S In C
+            K += 1
+            If LCase(a) = LCase(S) Then
+                grpNDX = K
+                Exit Function
+            End If
+        Next
+
+
+    End Function
+
+    Public Function safeFilename(ByVal a) As String
+        safeFilename = Replace(a, "\", "")
+        safeFilename = Replace(safeFilename, "..", "")
+    End Function
+
+
+    Public Function listNDX(ByRef C As List(Of String), ByRef a$) As Integer
+        Dim K As Integer = 0
+        listNDX = 0
+
+        For K = 0 To C.Count - 1
+            If C(K).ToString = a Then
+                listNDX = K + 1
+                Exit Function
+            End If
+        Next
+
+    End Function
+
+    Public Function jsonKV(k$, v$, Optional ByVal noQ As Boolean = False) As String
+        'stop using this function
+        Dim c$ = Chr(34)
+        'c$ = ""
+        Dim keYv$ = c + k + c + ": "
+        Dim vaLu$ = ""
+        If noQ = False Then vaLu = c + v + c Else vaLu = v
+        Return keYv + vaLu
+    End Function
+
+    Public Function arrNDX(ByRef A$(), ByRef matcH$) As Integer
+        'returns 0 if not found, otherwise NDX + 1
+        Dim K As Long
+        arrNDX = 0
+        For K = 0 To UBound(A)
+            If Trim(Str(A(K))) = matcH Then
+                arrNDX = K + 1
+                Exit Function
+            End If
+        Next
+    End Function
+    Public Function spaces(howmany As Integer) As String
+        spaces = ""
+        Dim K As Integer
+        For K = 1 To howmany
+            spaces += " "
+        Next
+    End Function
+    Public Function removeExtraSpaces(a) As String
+        removeExtraSpaces = ""
+        If Len(a) = 0 Then Exit Function
+        Dim lastSpace As Boolean = False
+
+        Dim K As Integer = 0
+        For K = 0 To Len(a) - 1
+            If lastSpace = False Then
+                removeExtraSpaces += Mid(a, K + 1, 1)
+            Else
+                If Mid(a, K + 1, 1) <> " " Then removeExtraSpaces += Mid(a, K + 1, 1)
+            End If
+            If Mid(a, K + 1, 1) = " " Then
+                lastSpace = True
+            End If
+        Next
+
+    End Function
+
+    Public Function countChars(a$, chr2Count$) As Integer
+        countChars = 0
+
+        Dim K As Integer = 0
+        For K = 0 To Len(a) - 1
+            If Mid(a, K + 1, 1) = chr2Count Then countChars += 1
+        Next
+    End Function
+
+    Public Function stripToFilename(ByVal fileN$) As String
+        'C:\Program Files\Checkmarx\Checkmarx Jobs Manager\Results\WebGoat.NET.Default 2014-10.9.2016-19.59.35.pdf
+        stripToFilename = ""
+
+        Do Until InStr(fileN, "\") = 0
+            fileN = Mid(fileN, InStr(fileN, "\") + 1)
+        Loop
+
+        stripToFilename = fileN
+
+    End Function
+
+    Public Function addSlash(ByVal a$) As String
+        addSlash = a
+        If Len(a) = 0 Then Exit Function
+
+        If Mid(a, Len(a), 1) <> "\" Then addSlash += "\"
+    End Function
+
+    Public Function getParentGroup(ByVal g$) As String
+        Dim a$ = StrReverse(g)
+        a = Mid(a, InStr(a, "\") + 1)
+        Return StrReverse(a)
+    End Function
+
+    Public Function stripLastWord(ByVal g$) As String
+        Dim a$ = StrReverse(g)
+        a = Mid(a, InStr(a, " ") + 1)
+        Return StrReverse(a)
+    End Function
+
+
+    Public Function assembleCollFromCLI(clI$) As Collection
+        Dim C As New Collection
+        ' takes windows dos-style dir output and makes sense of it for collection storage
+        Dim tempStr$ = clI
+        Dim K As Integer
+        Do Until InStr(tempStr, "  ") = 0
+            K = InStr(tempStr, "  ")
+            If Len(Mid(tempStr, 1, K - 1)) Then C.Add(Mid(tempStr, 1, K - 1))
+            tempStr = Replace(tempStr, Mid(tempStr, 1, K - 1) + "  ", "")
+            'Debug.Print(tempStr)
+        Loop
+        tempStr = LTrim(tempStr)
+        C.Add(Mid(tempStr, 1, InStr(tempStr, " ") - 1))
+        tempStr = Replace(tempStr, Mid(tempStr, 1, InStr(tempStr, " ") - 1), "")
+        C.Add(LTrim(tempStr))
+        Return C
+
+    End Function
+
+
+    Public Function CSVtoCOLL(ByRef csV$) As Collection
+        CSVtoCOLL = New Collection
+
+        Dim splitCHR$ = ","
+        If InStr(csV, splitCHR) = 0 Then splitCHR = ";"
+
+
+        Dim longS = Split(csV, splitCHR)
+
+        Dim K As Integer
+        For K = 0 To UBound(longS)
+            CSVtoCOLL.Add(longS(K))
+        Next
+
+    End Function
+
+
+    Public Function CSVFiletoCOLL(ByRef csV$) As Collection
+        CSVFiletoCOLL = New Collection
+        If Dir(csV) = "" Then Exit Function
+
+        'use file
+        Dim FF As Integer
+        FF = FreeFile()
+
+        FileOpen(FF, csV, OpenMode.Input)
+
+        Do Until EOF(FF) = True
+            CSVFiletoCOLL.Add(LineInput(FF))
+        Loop
+        FileClose(FF)
+
+    End Function
+
+    Public Sub safeKILL(ByRef fileN$)
+        If Dir(fileN) <> "" Then Kill(fileN)
+    End Sub
+
+
+    Public Function filePROP(fileN$, proP$) As String
+        filePROP = ""
+        If Dir(fileN) = "" Then Exit Function
+
+        If Len(proP) = 0 Then Exit Function
+
+        Dim FF As Integer = FreeFile()
+
+        FileOpen(FF, fileN, OpenMode.Input, OpenAccess.Read, OpenShare.Shared)
+
+        Dim a$ = ""
+
+        a = LineInput(FF)
+        Do Until a = "" Or EOF(FF) = True
+            If InStr(a, "=") = 0 Then GoTo nextLine
+
+            If UCase(proP) = Mid(a, 1, InStr(a, "=") - 1) Then
+                filePROP = Replace(a, proP + "=", "")
+            End If
+nextLine:
+            a = LineInput(FF)
+        Loop
+
+        If Len(a) = 0 Then GoTo closeHere
+
+        If UCase(proP) = Mid(a, 1, InStr(a, "=") - 1) Then
+            filePROP = Replace(a, proP + "=", "")
+        End If
+
+closeHere:
+
+        FileClose(FF)
+    End Function
+
+    Public Function allObjectsToList(fileN$) As List(Of String)
+        allObjectsToList = New List(Of String)
+        Dim C As New Collection
+        Call getAllObjNamesFromFile(fileN, C)
+
+        For Each A In C
+            allObjectsToList.Add(loadOBJfromFILE(fileN, A))
+        Next
+    End Function
+
+    Public Sub allObjectsWithProp(ByRef objS As List(Of String), prop$, propValue$, ByRef coll2Fill As Collection)
+        coll2Fill = New Collection
+
+        For Each O In objS
+            If UCase(objProp(O, UCase(prop))) = UCase(propValue) Then coll2Fill.Add(objProp(O, "NAME"))
+        Next
+    End Sub
+
+
+
+    Public Sub getAllObjNamesFromFile(fileN$, ByRef collOFnames As Collection)
+        collOFnames = New Collection
+
+        If Dir(fileN) = "" Then Exit Sub
+
+        Dim FF As Integer = FreeFile()
+
+        FileOpen(FF, fileN, OpenMode.Input, OpenAccess.Read, OpenShare.Shared)
+
+        Dim a$ = ""
+
+        a = LineInput(FF)
+        Do Until EOF(FF) = True
+            If UCase(Mid(a, 1, 5)) = "NAME=" Then
+                collOFnames.Add(Replace(a, "NAME" + "=", ""))
+            End If
+            a = LineInput(FF)
+        Loop
+
+        FileClose(FF)
+
+    End Sub
+
+    Public Function loadOBJfromFILE(fileN$, objName$) As String
+        loadOBJfromFILE = ""
+
+        If Dir(fileN) = "" Then Exit Function
+
+        If Len(objName) = 0 Then Exit Function
+
+        Dim FF As Integer = FreeFile()
+
+        FileOpen(FF, fileN, OpenMode.Input, OpenAccess.Read, OpenShare.Shared)
+
+        Dim a$ = ""
+        Dim buildStr$ = ""
+
+        Dim findSTR$ = "NAME=" + UCase(objName)
+
+        a = LineInput(FF)
+        Do Until UCase(a) = findSTR Or EOF(FF) = True
+nextLine:
+            a = LineInput(FF)
+        Loop
+
+        If UCase(a) = findSTR Then
+            Do Until a = "" Or EOF(FF) = True
+                buildStr += a + vbCrLf
+                a = LineInput(FF)
+            Loop
+        End If
+
+        loadOBJfromFILE = buildStr
+        FileClose(FF)
+
+
+    End Function
+
+    Public Function objProp(ByRef ObjString As String, propName$) As String
+        objProp = ""
+        Dim findS$ = UCase(propName) + "="
+
+        Dim O = Split(ObjString, vbCrLf)
+
+        If UBound(O) = 0 Then Exit Function
+
+        Dim K As Integer
+
+        For K = 0 To UBound(O)
+            If Mid(O(K), 1, Len(findS)) = UCase(propName) + "=" Then
+                'found object, return property
+                objProp = Mid(O(K), InStr(O(K), "=") + 1)
+                Exit Function
+            End If
+        Next
+
+    End Function
+
+
+    Public Function xlsDataType(dType$) As String
+        xlsDataType = "nonefound"
+        Select Case dType
+            Case "bigint", "int", "numeric", "float"
+                xlsDataType = "Numeric"
+            Case "datetime", "datetime2"
+                xlsDataType = "DateTime"
+            Case "date"
+                xlsDataType = "Date"
+            Case "time"
+                xlsDataType = "Time"
+            Case "bit"
+                xlsDataType = "Boolean"
+            Case "ntext", "nvarchar", "nchar", "varchar", "image", "uniqueidentifier", "real"
+                xlsDataType = "String"
+        End Select
+        If xlsDataType = "nonefound" Then
+            Debug.Print("No Def: " + dType)
+            xlsDataType = "String"
+        End If
+    End Function
+
+    Public Function xlsColName(colNum As Integer) As String
+        Dim d As Integer
+        Dim m As Integer
+        Dim name As String
+        d = colNum
+        name = ""
+        Do While (d > 0)
+            m = (d - 1) Mod 26
+            name = Chr(65 + m) + name
+            d = Int((d - m) / 26)
+        Loop
+        xlsColName = name
+    End Function
+
+    Public Function cleanJSON(json$) As String
+        If Len(json) = 0 Then Return ""
+
+        json = Mid(json, InStr(json, "["))
+        If Mid(json, Len(json), 1) = "}" Then json = Mid(json, 1, Len(json) - 1)
+        json = Replace(json, "null", "0")
+        'Console.WriteLine("CLEAN:" + vbCrLf + json)
+        Return json
+
+
+    End Function
+
+
+    Public Sub saveJSONtoFile(jsonString$, ByVal errFN$) ', ByRef add2zip As Collection)
+
+        Dim fileN$ = errFN
+
+        Call safeKILL(fileN)
+        Call streamWriterTxt(fileN, jsonString)
+
+        '        add2zip.Add(fileN)
+
+        GC.Collect()
+
+    End Sub
+
+    Public Function streamWriterTxt(fileN$, string2write$) As Boolean
+        streamWriterTxt = True
+        On Error GoTo errorCatch
+        Dim fS As New FileStream(fileN, FileMode.OpenOrCreate, FileAccess.Write)
+        Dim sW As New StreamWriter(fS)
+        sW.BaseStream.Seek(0, SeekOrigin.End)
+        sW.WriteLine(string2write$)
+        sW.Flush()
+        sW.Close()
+
+        sW = Nothing
+        fS = Nothing
+
+        Exit Function
+
+errorCatch:
+        streamWriterTxt = False
+        sW = Nothing
+        fS = Nothing
+
+
+    End Function
+
+    Public Function cleanJSONright(json$) As String
+        ' sometimes additional info 'stacked' onto json array.. doing this rather than customizing objects.. back up to ]
+        Dim K As Integer = Len(json)
+        Dim a$ = Mid(json, K, 1)
+
+        json = Mid(json, 1, InStr(json, "linkDataArray") - 1)
+
+        Do Until a$ = "]" Or Len(json) < 1
+            K = K - 1
+            json = Mid(json, 1, K)
+            a$ = Mid(json, K, 1)
+        Loop
+
+
+        Return json
+    End Function
+
+    Public Function getJSONObject(key$, json$) As String
+        On Error GoTo errorcatch
+
+        Dim sObject = JsonConvert.DeserializeObject(json)
+        Return sObject(key)
+
+errorcatch:
+        Return ""
+    End Function
+
+
+
+
+    Public NotInheritable Class Simple3Des
+        Private TripleDes As TripleDESCryptoServiceProvider
+
+        Private Function TruncateHash(ByVal key As String, ByVal length As Integer) As Byte()
+
+            Dim sha1 As New SHA1CryptoServiceProvider
+
+            ' Hash the key.
+            Dim keyBytes() As Byte =
+                System.Text.Encoding.Unicode.GetBytes(key)
+            Dim hash() As Byte = sha1.ComputeHash(keyBytes)
+
+            ' Truncate or pad the hash.
+            ReDim Preserve hash(length - 1)
+            Return hash
+        End Function
+
+        Sub New(ByVal key As String)
+            ' Initialize the crypto provider.
+            TripleDes = New TripleDESCryptoServiceProvider
+            TripleDes.Key = TruncateHash(key, TripleDes.KeySize \ 8)
+            TripleDes.IV = TruncateHash("", TripleDes.BlockSize \ 8)
+        End Sub
+
+        Private Function EncryptData(ByVal plaintext As String) As String
+
+            ' Convert the plaintext string to a byte array. 
+            Dim plaintextBytes() As Byte =
+                System.Text.Encoding.Unicode.GetBytes(plaintext)
+
+            ' Create the stream. 
+            Dim ms As New System.IO.MemoryStream
+            ' Create the encoder to write to the stream. 
+            Dim encStream As New CryptoStream(ms,
+                TripleDes.CreateEncryptor(),
+                System.Security.Cryptography.CryptoStreamMode.Write)
+
+            ' Use the crypto stream to write the byte array to the stream.
+            encStream.Write(plaintextBytes, 0, plaintextBytes.Length)
+            encStream.FlushFinalBlock()
+
+            ' Convert the encrypted stream to a printable string. 
+
+
+            Return Convert.ToBase64String(ms.ToArray)
+
+        End Function
+
+        Private Function DecryptData(ByVal encryptedtext As String) As String
+
+            ' Convert the encrypted text string to a byte array. 
+            Dim encryptedBytes() As Byte = Convert.FromBase64String(encryptedtext)
+
+            ' Create the stream. 
+            Dim ms As New System.IO.MemoryStream
+            ' Create the decoder to write to the stream. 
+            Dim decStream As New CryptoStream(ms,
+                TripleDes.CreateDecryptor(),
+                System.Security.Cryptography.CryptoStreamMode.Write)
+
+            ' Use the crypto stream to write the byte array to the stream.
+            decStream.Write(encryptedBytes, 0, encryptedBytes.Length)
+            decStream.FlushFinalBlock()
+
+            ' Convert the plaintext stream to a string. 
+            Return System.Text.Encoding.Unicode.GetString(ms.ToArray)
+        End Function
+
+        Public Function Decode(cipher As String) As String
+            Try
+                Return DecryptData(cipher)
+            Catch ex As CryptographicException
+                Throw New Exception(ex.Message)
+            End Try
+
+        End Function
+
+        Public Function Encode(txt As String) As String
+            Try
+                Return EncryptData(txt)
+            Catch ex As CryptographicException
+                Throw New Exception(ex.Message)
+            End Try
+        End Function
+
+    End Class
+
+
+End Module
---
+++ TMutil/tmWrapper.vb
@@ -0,0 +1,241 @@
+﻿Imports RestSharp
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.Web.HttpUtility
+Public Class tmWrapper
+
+End Class
+
+Public Class genJSONobj
+    Public isSuccess$
+    Public Message$
+    Public Data$
+End Class
+Public Class TM_Client
+    Public lastError$ = ""
+    Public tmFQDN$ 'ie http://localhost https://myserver.myzone.com
+    Public isConnected As Boolean
+    Private slToken$
+
+    Public Sub New(fqdN$, uN$, pw$)
+
+        tmFQDN = fqdN
+
+        Me.isConnected = False
+
+        'Console.WriteLine("New TM_Client activated: " + Replace(tmFQDN, "https://", ""))
+
+        Dim client = New RestClient(fqdN + "/token")
+        Dim request = New RestRequest(Method.POST)
+        Dim response As IRestResponse
+
+        client.Timeout = -1
+
+        request.RequestFormat = DataFormat.None
+
+        request.AddHeader("Content-Type", "application/x-www-form-urlencoded")
+        request.AddParameter("username", uN)
+        request.AddParameter("password", pw)
+        request.AddParameter("grant_type", "password")
+
+        response = client.Execute(request)
+        'Console.WriteLine(response.Content)
+
+        If response.IsSuccessful = True Then
+            Me.isConnected = True
+            Dim O As JObject = JObject.Parse(response.Content)
+            slToken = O.SelectToken("access_token")
+            Exit Sub
+        End If
+
+        If IsNothing(response.ErrorMessage) = False Then
+            lastError = response.ErrorMessage.ToString
+        Else
+            lastError = response.Content
+        End If
+
+        Console.WriteLine(lastError)
+        'Console.WriteLine("Retrieved access token")
+    End Sub
+
+    Private Function getAPIData(ByVal urI$, Optional ByVal usePOST As Boolean = False, Optional ByVal addJSONbody$ = "") As String
+        getAPIData = ""
+        If isConnected = False Then Exit Function
+
+        Dim client = New RestClient(tmFQDN + urI)
+        Dim request As RestRequest
+        If usePOST = False Then request = New RestRequest(Method.GET) Else request = New RestRequest(Method.POST)
+
+        Dim response As IRestResponse
+
+        request.AddHeader("Authorization", "Bearer " + slToken)
+        request.AddHeader("Accept", "application/json")
+
+        If Len(addJSONbody) Then
+            request.AddHeader("Content-Type", "application/json")
+            request.AddHeader("Accept-Encoding", "gzip, deflate, br")
+            request.AddHeader("Connection", "keep-alive")
+            request.AddParameter("application/json", addJSONbody, ParameterType.RequestBody)
+        End If
+
+        response = client.Execute(request)
+
+        Dim O As JObject = JObject.Parse(response.Content)
+
+        If CBool(O.SelectToken("IsSuccess")) = False Then
+            getAPIData = "ERROR:Could not retrieve " + urI
+            Exit Function
+        End If
+
+        Return O.SelectToken("Data").ToString
+    End Function
+    Public Function getGroups() As List(Of tmGroups)
+        getGroups = New List(Of tmGroups)
+        Dim jsoN$ = getAPIData("/api/groups")
+
+        getGroups = JsonConvert.DeserializeObject(Of List(Of tmGroups))(jsoN)
+
+    End Function
+
+    Public Function getProjectsOfGroup(G As tmGroups) As List(Of tmProjInfo)
+        getProjectsOfGroup = New List(Of tmProjInfo)
+
+        Dim jBody$ = ""
+        jBody = JsonConvert.SerializeObject(G)
+        Dim jsoN$ = getAPIData("/api/group/projects", True, jBody$)
+
+        getProjectsOfGroup = JsonConvert.DeserializeObject(Of List(Of tmProjInfo))(jsoN)
+    End Function
+
+    Public Function getProject(projID As Long) As tmModel
+        getProject = New tmModel
+
+        Dim jsoN$ = getAPIData("/api/diagram/" + projID.ToString + "/components")
+
+        Dim nD As JObject = JObject.Parse(jsoN)
+        Dim nodeJSON$ = nD.SelectToken("Model").ToString
+
+        Dim nodeS As List(Of tmNodeData)
+        nodeJSON = cleanJSON(nodeJSON)
+        nodeJSON = cleanJSONright(nodeJSON)
+
+        Dim SS As New JsonSerializerSettings
+        SS.NullValueHandling = NullValueHandling.Ignore
+
+        nodeS = JsonConvert.DeserializeObject(Of List(Of tmNodeData))(nodeJSON, SS)
+
+        getProject = JsonConvert.DeserializeObject(Of tmModel)(jsoN)
+        getProject.Nodes = nodeS
+    End Function
+
+End Class
+
+Public Class tmGroups
+        Public Id As Long
+        Public Name$
+        Public isSystem As Boolean
+        Public ParentGroupId As Integer
+        Public ParentGroupName$
+        Public Department$
+        Public DepartmentId$
+        Public Guid As System.Guid
+        Public Projects$
+        Public GroupUsers$
+        Public AllProjInfo As List(Of tmProjInfo)
+    End Class
+Public Class tmProjInfo
+    Public Id As Long
+    Public Name$
+    Public Version$
+    'Public Guid As System.Guid
+    Public isInternal As Boolean
+    Public RiskId As Integer
+    'Public RiskName As Integer
+    Public [Type] As String
+    Public CreatedByUserEmail$
+    Public Model As tmModel
+End Class
+
+Public Class tmProjThreat
+        Public Id As Long
+        'Public Guid as system.guid 'nulls
+        Public Name$
+        Public Description$
+        Public RiskId As Integer
+        Public RiskName$
+        Public LibraryId As Integer
+        Public Labels$
+        Public Reference$
+        Public Automated As Boolean
+        'public ThreatAttributes$ 'nulls
+        Public StatusName$
+        Public IsHidden As Boolean
+        Public CompanyId As Integer
+        'Public SharingType$
+        'Public ReadOnly As Boolean
+        Public isDefault As Boolean
+        Public DateCreated$
+        Public LastUpdated$
+        Public DepartmentId As Integer
+        Public DepartmentName$
+        'public Version$ 
+        Public IsSystemDepartment As Boolean
+
+    End Class
+
+    Public Class tmProjSecReq
+        Public Id As Long
+        Public Name$
+        Public Description$
+        Public RiskId As Integer
+        Public IsCompensatingControl As Boolean
+        Public RiskName$
+        Public Labels$
+        Public LibraryId As Integer
+        'Public Guid as System.Guid
+        Public StatusName$
+        Public SourceName$
+        Public IsHidden As Boolean
+    End Class
+
+    Public Class tmNodeData
+        Public key As Single
+        Public category$
+        Public Name$
+        Public [type] As String
+        Public ImageURI$
+        Public NodeId As Integer
+        Public ComponentId As Integer
+        Public group As Single
+        Public Location$
+        Public color$
+        Public BorderColor$
+        Public BackgroundColor$
+        Public TitleColor$
+        Public TitleBackgroundColor$
+        Public isGroup As Boolean
+        Public isGraphExpanded As Boolean
+        Public FullName$
+        Public cpeid$ ' As Integer
+        Public networkComponentId As Integer
+        Public BorderThickness As Decimal
+        Public AllowMove As Boolean
+        Public LayoutWidth As Single
+        Public LayoutHeight As Single
+        Public componentProperties$
+        Public listThreats As List(Of tmProjThreat)
+        Public listSecurityRequirements As List(Of tmProjSecReq)
+    Public ComponentTypeName$
+    Public ComponentName$
+    Public Id As Long
+    Public threatCount As Integer
+End Class
+
+    Public Class tmModel
+        Public Id As Long
+        Public Name$
+        Public IsAppliedForApproval As Boolean
+        Public Guid As System.Guid
+        Public Nodes As List(Of tmNodeData)
+    End Class
+
